bb7ecf481690ae75afe6b69b488f1193610cbf07
cccccmake <kechou.dai@gmail.com>
2023-12-05 13:18:36 +0100

    update

---
+++ src/main/java/com/imooc/oa/service/UserService.java
@@ -0,0 +1,20 @@
+package com.imooc.oa.service;
+
+import com.imooc.oa.entity.User;
+import com.imooc.oa.mapper.UserMapper;
+import com.imooc.oa.service.exception.LoginException;
+
+public class UserService {
+    private UserMapper userMapper = new UserMapper();
+
+    public User checkLogin(String username, String password) {
+        User user = userMapper.selectByUsername(username);
+        if (user == null) {
+            throw new LoginException("User does not exist!");
+        }
+        if(!password.equals(user.getPassword())){
+            throw new LoginException("Password error!");
+        }
+        return user;
+    }
+}
